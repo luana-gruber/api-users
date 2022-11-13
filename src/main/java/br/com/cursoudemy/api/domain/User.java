002d669b5373fdde9a5a54d0cea251be8de16e0d
luana-gruber <luana_gruber@hotmail.com>
2022-11-13 14:11:33 -0300

    Classe usuário

---
+++ src/main/java/br/com/cursoudemy/api/domain/User.java
@@ -0,0 +1,21 @@
+package br.com.cursoudemy.api.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+    private String password;
+}
