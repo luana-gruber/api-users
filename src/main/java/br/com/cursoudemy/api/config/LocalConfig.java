66718299243ea79a6a9b20a66886c46cf103fe93
luana-gruber <luana_gruber@hotmail.com>
2022-11-13 19:15:13 -0300

    Rota findALL

--- src/main/java/br/com/cursoudemy/api/config/LocalConfig.java
+++ src/main/java/br/com/cursoudemy/api/config/LocalConfig.java
@@ -4,21 +4,22 @@ import br.com.cursoudemy.api.domain.User;
 import br.com.cursoudemy.api.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import java.util.List;
 
 @Configuration
+@ComponentScan(basePackageClasses = {UserRepository.class})
 @Profile("local")
 public class LocalConfig {
     @Autowired
     private UserRepository repository;
-
     @Bean
     public void startDB(){
-        User u1 = new User(1, "Luana", "luana@hotmail.com", "1234");
-        User u2 = new User(2, "Maria", "maria@hotmail.com", "1234");
+        User u1 = new User(null, "Luana", "luana@hotmail.com", "1234");
+        User u2 = new User(null, "Maria", "maria@hotmail.com", "1234");
 
         repository.saveAll(List.of(u1,u2));
 
--- src/main/java/br/com/cursoudemy/api/services/UserService.java
+++ src/main/java/br/com/cursoudemy/api/services/UserService.java
@@ -2,7 +2,9 @@ package br.com.cursoudemy.api.services;
 
 import br.com.cursoudemy.api.domain.User;
 
-public interface UserService {
+import java.util.List;
 
+public interface UserService {
     User findById(Integer id);
+    List<User> findAll();
 }
