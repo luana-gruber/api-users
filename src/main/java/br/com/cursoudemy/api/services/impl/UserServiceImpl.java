5f9d7c50b610ce61a5c5c9adf528e828b6b855c5
luana-gruber <luana_gruber@hotmail.com>
2022-11-14 20:59:08 -0300

    Exceção para e-mail já cadastrado

--- src/main/java/br/com/cursoudemy/api/services/impl/UserServiceImpl.java
+++ src/main/java/br/com/cursoudemy/api/services/impl/UserServiceImpl.java
@@ -4,6 +4,7 @@ import br.com.cursoudemy.api.domain.Users;
 import br.com.cursoudemy.api.domain.dto.UserDTO;
 import br.com.cursoudemy.api.repositories.UserRepository;
 import br.com.cursoudemy.api.services.UserService;
+import br.com.cursoudemy.api.services.exceptions.DataIntegratyViolationException;
 import br.com.cursoudemy.api.services.exceptions.ObjectNotFoundException;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,6 +32,14 @@ public class UserServiceImpl implements UserService {
     }
     @Override
     public Users create(UserDTO obj) {
+        findByEmail(obj);
         return repository.save(mapper.map(obj, Users.class));
+    }
+
+    private void findByEmail(UserDTO obj){
+        Optional<Users> user = repository.findByEmail(obj.getEmail());
+        if(user.isPresent()){
+            throw new DataIntegratyViolationException("E-mail já cadastrado no sistema");
+        }
     }
 }
